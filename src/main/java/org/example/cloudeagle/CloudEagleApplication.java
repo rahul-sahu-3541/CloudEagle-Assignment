83b9b5395b892958b0704da34d9c1d4475398c81
Rahul Sahu <rahulsahuiitb@gmail.com>
2025-11-16 12:00:25 +0530

    commit - add DropboxOAuth.java

---
+++ src/main/java/org/example/cloudeagle/CloudEagleApplication.java
@@ -0,0 +1,13 @@
+package org.example.cloudeagle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEagleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEagleApplication.class, args);
+    }
+
+}
